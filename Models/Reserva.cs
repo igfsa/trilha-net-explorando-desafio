900781667ea47a2e0619a14439c28dc1ea159e7b
Ícaro Sanches <igfsaicaro@gmail.com>
2025-07-23 14:22:50 -0300

    Implementação da solução básica do desafio proposto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
+            // Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // IMPLEMENTADO
             if (this.Suite.Capacidade >= hospedes.Count())
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // IMPLEMENTADO
                 throw new Exception("Ocorreu um erro. A quantidade de hóspedes não pode ser maior que a capacidade da suíte.");
             }
         }
@@ -36,20 +36,20 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // IMPLEMENTADO
             return this.Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
+            // Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            // IMPLEMENTADO
             decimal valor = this.DiasReservados * this.Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
+            // IMPLEMENTADO
             if (this.DiasReservados >= 10)
             {
                 valor *= Convert.ToDecimal(0.9);
